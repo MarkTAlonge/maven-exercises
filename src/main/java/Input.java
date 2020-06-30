84340424927feb2727dff87ffc02a5a658f67aad
Mark Alonge <Mark.T.Alonge@gmail.com>
2020-06-30 08:52:56 -0500

    update to the maven exercise

---
+++ src/main/java/Input.java
@@ -0,0 +1,20 @@
+import org.apache.commons.lang3.arch.*;
+import javax.swing.*;
+import java.util.*;
+
+
+
+public class Input {
+    public static void main(String[] args) {
+
+             Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter Input");
+            String str = sc.nextLine();
+           String IsNumeric;
+           String SwapCase;
+           String Reverse;
+
+        System.out.println("you enterd " + str);
+
+    }
+}
